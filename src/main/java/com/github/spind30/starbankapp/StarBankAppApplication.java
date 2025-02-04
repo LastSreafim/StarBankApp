fc6765c83c590598befc3db880c4f652b4531c87
Nazar <epoxasmute@mail.ru>
2025-02-04 21:51:02 +0300

    Initial commit

---
+++ src/main/java/com/github/spind30/starbankapp/StarBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.spind30.starbankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBankAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarBankAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/spind30/starbankapp/StarBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.spind30.starbankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarBankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
